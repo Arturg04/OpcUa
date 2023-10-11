280774ae06533f98052f3633849e65a18a5cfdd7
Artur <artur.13.goncalves@gmail.com>
2023-10-11 14:41:35 +0100

    up

--- OpcLib.h
+++ OpcLib.h
@@ -1,47 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   OpcLib.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-pinh <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 11:30:46 by Arturg04          #+#    #+#             */
+/*   Updated: 2023/10/11 14:31:45 by ade-pinh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef OPCLIB_H
 # define OPCLIB_H
 
 # include <unistd.h>
+# include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include <pthread.h>
+# include <signal.h>
+# include "Open62541_files/open62541.h"
+# include "libft.h"
 
-# include <open62541/types.h>
-# include <open62541/client.h>
-# include <open62541/server.h>
-# include <open62541/client_subscriptions.h>
-# include <open62541/client_highlevel_async.h>
-# include <open62541/client_config_default.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1024
+# endif
 
+# define MAX_FILES 1024
 
-# include <libxml/parser.h>
-# include <libxml/tree.h>
+typedef	struct s_node
+{
+	int	index;
+	int	identifier;
+} t_node;
 
-# include <sqlite3.h>
-
-struct	TagInfo {
-	char name[50];
-	char node[50];
-	char tag_type[50];
-};
+typedef struct s_TagInfo {
+	char				*name;
+	t_node				*node;
+	char				*tag_type;
+	int32_t				*subid;
+	struct s_TagInfo	*next;
+	struct s_TagInfo	*prev;
+}	t_TagInfo;
 
 
 // Server Comm
-int		isServerReachable(const char *serverAddress);
-int		connectToOpcServer(UA_Client **clientPtr, const char *serverUrl);
-void	disconnectFromOpcServer(UA_Client *client);
-
-// Server data
-UA_StatusCode createSubscriptions(UA_Client *client, struct TagInfo tags[], int tagCount);
+UA_Client	*ConnectToOpcServer(const char *serverUrl);
+void	DisconnectFromOpcServer(UA_Client *client);
+void	CreateSubscription(UA_Client *client, t_TagInfo *Tag);
 
 
-// db
-//void	addTags(struct	TagInfo Tags[], int size);
-//void	updateValue(char *node, int value);
 
+//xml
+t_TagInfo	*readxml(char *filepath);
 
-// xml
-int		parseXmlFile(const char *filename, struct TagInfo tags[], int maxTags);
-void	printTags(struct TagInfo tags[], int tagCount);
 
 #endif
