3d5659e9af84f8d0bf92f0d7248606ad1539eac7
Artur <artur.13.goncalves@gmail.com>
2023-10-12 17:25:15 +0100

    uo

--- OpcService.c
+++ OpcService.c
@@ -6,32 +6,23 @@
 /*   By: ade-pinh <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:45:43 by ade-pinh          #+#    #+#             */
-/*   Updated: 2023/10/12 15:30:14 by ade-pinh         ###   ########.fr       */
+/*   Updated: 2023/10/12 17:04:14 by ade-pinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/OpcLib.h"
 
-UA_Boolean g_running = true;
 
-static void	stop_handler(int signal)
-{
-	(void)signal;
-	UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
-	g_running = false;
-}
 
 int	main(void)
 {
-	char		*dbpath;
-	char		*filepath;
-	char		*url;
-	UA_Client	*client;
-	t_TagInfo	*tags;
-	t_TagInfo	*tagsptr;
+	char					*dbpath;
+	char					*filepath;
+	char					*url;
+	UA_Client				*client;
+	t_TagInfo				*tags;
+	t_TagInfo				*tagsptr;
 
-	signal(SIGINT, stop_handler);
-	signal(SIGTERM, stop_handler);
 	dbpath = ft_strdup("IEM.db");
 	filepath = ft_strdup("tags.xml");
 	url = ft_strdup("opc.tcp://192.168.10.1:4840");
@@ -53,8 +44,6 @@ int	main(void)
 		tagsptr = tagsptr->next;
 	}
 	client = connect_opc_server(url, tags);
-	while (g_running)
-		UA_Client_run_iterate(client, 100);
 	disconnect_opc_server(client);
 	close_db();
 	free(filepath);
--- src/OpcComm.c
+++ src/OpcComm.c
@@ -6,15 +6,38 @@
 /*   By: ade-pinh <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:43:20 by ade-pinh          #+#    #+#             */
-/*   Updated: 2023/10/12 16:27:03 by ade-pinh         ###   ########.fr       */
+/*   Updated: 2023/10/12 17:23:50 by ade-pinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/OpcLib.h"
 
+static UA_Boolean	g_running = true;
+static int			g_server = 0;
+static char			*g_url;
+
+static void	stop_handler(int signal)
+{
+	(void)signal;
+	UA_LOG_INFO(UA_Log_Stdout, UA_LOGCATEGORY_USERLAND, "received ctrl-c");
+	g_running = false;
+}
+
 void	connection_inactivity_callback(UA_Client *client)
 {
+	t_TagInfo		*tags;
+	UA_StatusCode	retval;
 
+	g_server = 0;
+	tags = (t_TagInfo *)UA_Client_getConfig(client)->clientContext;
+	printf("Server disconnected. Attempting to reconnect...\n");
+	UA_Client_disconnect(client);
+	retval = UA_Client_connect(client, g_url);
+	while (retval != UA_STATUSCODE_GOOD)
+	{
+		printf("Failed to reconnect.\n");
+		retval = UA_Client_connect(client, g_url);
+	}
 }
 
 void	connection_state_callback(UA_Client *client,
@@ -23,29 +46,14 @@ void	connection_state_callback(UA_Client *client,
 						UA_StatusCode connectstatus)
 {
 	t_TagInfo	*tags;
-	char	*url;
 
-	if (sessionstate == UA_CONNECTIONSTATE_OPENING)
-		printf("Connection Open!\n");
-	else if (sessionstate == UA_CONNECTIONSTATE_CLOSED)
-	{
-		printf("Server disconnected. Attempting to reconnect...\n");
-		url = (char	*)&UA_Client_getConfig(client)->applicationUri;
-		if (!*url)
-			return ;
-		connectstatus = UA_Client_connect(client, url);
-		while (connectstatus != UA_STATUSCODE_GOOD)
-		{
-			printf("Failed to reconnect.\n");
-			connectstatus = UA_Client_connect(client, url);
-		}
-	}
 	if (connectstatus == UA_STATUSCODE_GOOD && sessionstate == UA_SESSIONSTATE_ACTIVATED && chanelstate == UA_SECURECHANNELSTATE_OPEN)
 	{
 		printf("Connection Established.\n");
 		tags = (t_TagInfo *)UA_Client_getConfig(client)->clientContext;
 		while (tags)
 			tags = create_subscription(client, tags);
+		g_server = 1;
 	}
 }
 
@@ -53,7 +61,11 @@ UA_Client	*connect_opc_server(const char *serverUrl, t_TagInfo *tags)
 {
 	UA_Client		*client;
 	UA_StatusCode	retval;
+	g_url = ft_strdup(serverUrl);
 
+
+	signal(SIGINT, stop_handler);
+	signal(SIGTERM, stop_handler);
 	client = UA_Client_new();
 	UA_ClientConfig_setDefault(UA_Client_getConfig(client));
 	UA_Client_getConfig(client)->stateCallback = connection_state_callback;
@@ -62,6 +74,18 @@ UA_Client	*connect_opc_server(const char *serverUrl, t_TagInfo *tags)
 	UA_Client_getConfig(client)->connectivityCheckInterval = 10;
 	UA_Client_getConfig(client)->clientContext = tags;
 	retval = UA_Client_connect(client, serverUrl);
+	while (g_running)
+	{
+		if(g_server)
+		{
+			if (g_server == 1)
+			{
+				g_server = 2;
+				sleep(10);
+			}
+			UA_Client_run_iterate(client, 20);
+		}
+	}
 	return (client);
 }
 
