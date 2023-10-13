de894f6c8e8f2565d3acc807e90b15e999061769
Artur <artur.13.goncalves@gmail.com>
2023-10-13 08:33:54 +0100

    ok

--- src/OpcComm.c
+++ src/OpcComm.c
@@ -6,11 +6,17 @@
 /*   By: ade-pinh <artur.13.goncalves@gmail.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/11 13:43:20 by ade-pinh          #+#    #+#             */
-/*   Updated: 2023/10/12 17:23:50 by ade-pinh         ###   ########.fr       */
+/*   Updated: 2023/10/13 08:32:45 by ade-pinh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/OpcLib.h"
+
+void	connection_inactivity_callback(UA_Client *client);
+void	connection_state_callback(UA_Client *client,
+						UA_SecureChannelState chanelstate ,
+						UA_SessionState sessionstate,
+						UA_StatusCode connectstatus);
 
 static UA_Boolean	g_running = true;
 static int			g_server = 0;
@@ -23,6 +29,18 @@ static void	stop_handler(int signal)
 	g_running = false;
 }
 
+UA_Client	*client_config(UA_Client *client, t_TagInfo *tags)
+{
+	client = UA_Client_new();
+	UA_ClientConfig_setDefault(UA_Client_getConfig(client));
+	UA_Client_getConfig(client)->stateCallback = connection_state_callback;
+	UA_Client_getConfig(client)->inactivityCallback = connection_inactivity_callback;
+	UA_Client_getConfig(client)->requestedSessionTimeout = 0;
+	UA_Client_getConfig(client)->connectivityCheckInterval = 10;
+	UA_Client_getConfig(client)->clientContext = tags;
+	return (client);
+}
+
 void	connection_inactivity_callback(UA_Client *client)
 {
 	t_TagInfo		*tags;
@@ -32,6 +50,8 @@ void	connection_inactivity_callback(UA_Client *client)
 	tags = (t_TagInfo *)UA_Client_getConfig(client)->clientContext;
 	printf("Server disconnected. Attempting to reconnect...\n");
 	UA_Client_disconnect(client);
+	UA_Client_delete(client);
+	client = client_config(client, tags);
 	retval = UA_Client_connect(client, g_url);
 	while (retval != UA_STATUSCODE_GOOD)
 	{
@@ -54,8 +74,11 @@ void	connection_state_callback(UA_Client *client,
 		while (tags)
 			tags = create_subscription(client, tags);
 		g_server = 1;
+			while (g_server)
+				UA_Client_run_iterate(client, 20);
 	}
 }
+
 
 UA_Client	*connect_opc_server(const char *serverUrl, t_TagInfo *tags)
 {
@@ -66,26 +89,9 @@ UA_Client	*connect_opc_server(const char *serverUrl, t_TagInfo *tags)
 
 	signal(SIGINT, stop_handler);
 	signal(SIGTERM, stop_handler);
-	client = UA_Client_new();
-	UA_ClientConfig_setDefault(UA_Client_getConfig(client));
-	UA_Client_getConfig(client)->stateCallback = connection_state_callback;
-	UA_Client_getConfig(client)->inactivityCallback = connection_inactivity_callback;
-	UA_Client_getConfig(client)->requestedSessionTimeout = 0;
-	UA_Client_getConfig(client)->connectivityCheckInterval = 10;
-	UA_Client_getConfig(client)->clientContext = tags;
+	client = client_config(client, tags);
 	retval = UA_Client_connect(client, serverUrl);
 	while (g_running)
-	{
-		if(g_server)
-		{
-			if (g_server == 1)
-			{
-				g_server = 2;
-				sleep(10);
-			}
-			UA_Client_run_iterate(client, 20);
-		}
-	}
 	return (client);
 }
 
